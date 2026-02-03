50857201659a335a4440ba45fc2df79264c4e7d8
KseniiaS <ksenija3101993@gmail.com>
2026-02-03 15:48:35 +0300

    Выполнена базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    var counterValue: Int = 0
+    @IBAction func tapButton(_ sender: Any) {
+        repeat {
+            counterValue += 1
+        }
+        while counterValue > 1000
+        counterLabel.text = "Значение счетчика:  \(counterValue)"
     }
 
-
+    
 }
 
