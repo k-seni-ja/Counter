ee2cf21ab9b9e6e8670697ad2898cd5a0432380a
KseniiaS <ksenija3101993@gmail.com>
2026-02-05 20:55:49 +0300

    Изменены модификаторы  доступа

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,69 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counterValue: Int = 0
     
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var resetValueButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var storyTextView: UITextView!
-    var counterValue: Int = 0
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var resetValueButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var storyTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
     
-    
-    @IBAction func minusCounter(_ sender: Any) {
-        //настройка Даты и времени
+    @IBAction private func minusCounter(_ sender: Any) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         let currentDate = dateFormatter.string(from: Date())
-        // логика окна счетчика (UI Label)
+        
         if counterValue > 0 {
             counterValue -= 1
         }
         counterLabel.text = "Значение счетчика:  \(counterValue)"
-        //логика окна истории (UI TextView)
+        
         if counterValue != 0 {
             storyTextView.text += "\n [\(currentDate)]: значение изменено на -1"
         }
         else{
             storyTextView.text += "\n [\(currentDate)]: попытка уменьшить значение счётчика ниже 0"
         }
-        //настройка скроллинга
+        
         let range = NSRange(location: storyTextView.text.count - 1, length: 1)
         storyTextView.scrollRangeToVisible(range)
     }
     
     
-    @IBAction func plusCounter(_ sender: Any) {
-        //настройка Даты и времени
+    @IBAction private func plusCounter(_ sender: Any) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         let currentDate = dateFormatter.string(from: Date())
-        // логика окна счетчика (UI Label)
+        
         counterValue += 1
         counterLabel.text = "Значение счетчика:  \(counterValue)"
-        //логика окна истории (UI TextView)
+        
         storyTextView.text += "\n [\(currentDate)]: значение изменено на +1"
-        //настройка скроллинга
+        
         let range = NSRange(location: storyTextView.text.count - 1, length: 1)
         storyTextView.scrollRangeToVisible(range)
     }
     
     
-    @IBAction func resetValueCounter(_ sender: Any) {
-        //настройка Даты и времени
+    @IBAction private func resetValueCounter(_ sender: Any) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         let currentDate = dateFormatter.string(from: Date())
-        // логика окна счетчика (UI Label)
+        
         counterValue = 0
         counterLabel.text = "Значение счетчика:  \(counterValue)"
-        //логика окна истории (UI TextView)
+        
         storyTextView.text += "\n [\(currentDate)]: значение сброшено"
-        //настройка скроллинга
+        
         let range = NSRange(location: storyTextView.text.count - 1, length: 1)
         storyTextView.scrollRangeToVisible(range)
     }
